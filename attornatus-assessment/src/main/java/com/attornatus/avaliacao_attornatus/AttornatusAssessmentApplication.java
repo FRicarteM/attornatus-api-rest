9fc0a01eee32c565ee554b08fc4747196d4b2ec0
Fabricio Ricarte <fabricio_ricarte@hotmail.com>
2023-01-27 23:38:40 -0300

    Commit inicial do projeto. Estrutura do Projeto mais dependencias

---
+++ attornatus-assessment/src/main/java/com/attornatus/avaliacao_attornatus/AttornatusAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.avaliacao_attornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusAssessmentApplication.class, args);
+	}
+
+}
---
+++ attornatus-assessment/src/test/java/com/attornatus/avaliacao_attornatus/AttornatusAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.avaliacao_attornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
